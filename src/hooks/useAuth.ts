51d74d816713ecda1901b4abd8c160d2886d9d47
Shota Ito <shota.110.free.style@gmail.com>
2021-11-02 01:06:35 -0400

    Login message using toast from ChakraUI

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,14 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+
+  // const { function name inside of useMessage } = useMessage(name of export function)();
+  const { showMessage } = useMessage();
+
   const [loading, setLoading] = useState(false);
 
   const login = useCallback(
@@ -15,15 +20,27 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({
+              title: "Logged in successfully!",
+              status: "success"
+            });
             history.push("/home");
           } else {
-            alert("The user does not exist.");
+            showMessage({
+              title: "The user does not exist.",
+              status: "error"
+            });
           }
         })
-        .catch(() => alert("You are not albe to login"))
+        .catch(() =>
+          showMessage({
+            title: "Error! Please try again.",
+            status: "error"
+          })
+        )
         .finally(() => setLoading(false));
     },
-    [history]
+    [history, showMessage]
   );
-  return { login };
+  return { login, loading };
 };
